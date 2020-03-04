4d72a5e7bc0dc7186bbac6afb4f31d899a87e315
dnsdhcp <cjv976@gmail.com>
2020-03-04 23:16:12 +0800

    OK?

--- OOP/OOP/OOP.cpp
+++ OOP/OOP/OOP.cpp
@@ -8,5 +8,14 @@ bool Guess(int number)
 }
 int main(array<System::String^>^ args)
 {
+    int guess;
+
+    do
+    {
+        std::cout << "Choose a number between 1 - 100 :";
+        std::cin >> guess;
+    }
+    while (!Guess(guess));
+
     return 0;
 }
