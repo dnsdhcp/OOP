b74a4fe2735b3cadeb7b9336498beee235cec571
dnsdhcp <cjv976@gmail.com>
2020-03-04 23:17:43 +0800

    ???

--- OOP/OOP/OOP.cpp
+++ OOP/OOP/OOP.cpp
@@ -4,6 +4,22 @@
 using namespace System;
 bool Guess(int number)
 {
+    static int target = -1;
+
+    if (target == -1)
+    {
+        Random r;
+        target = r.Next() % 100 + 1;
+    }
+
+    if (number == target)
+    {
+        std::cout << "Correct !!";
+        target = -1;
+        return true;
+    }
+    else std::cout << "Wrong" << std::endl;
+
     return false;
 }
 int main(array<System::String^>^ args)
@@ -12,7 +28,6 @@ int main(array<System::String^>^ args)
 
     do
     {
-        std::cout << "Choose a number between 1 - 100 :";
         std::cin >> guess;
     }
     while (!Guess(guess));
